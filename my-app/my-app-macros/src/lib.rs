5f7e14918dfceec427280e0aa16bac0636badc53
kien6034 <trinhtrungkientb99@gmail.com>
2024-07-18 16:04:20 +0700

    macro: derive custom struct

--- my-app/my-app-macros/src/lib.rs
+++ my-app/my-app-macros/src/lib.rs
@@ -5,8 +5,10 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::Data;
 
+mod custom_model;
+
 #[proc_macro_derive(IntoStringHashMap)]
-pub fn derive_into_hash_map(item: TokenStream) -> TokenStream {
+pub fn derive_into_string_hash_map(item: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(item as syn::DeriveInput);
 
     let struct_identifier = &input.ident;
@@ -37,6 +39,10 @@ pub fn derive_into_hash_map(item: TokenStream) -> TokenStream {
                 hash_map.insert("last_name".to_string(), String::from(value.last_name));
             */
 
+            //  // Implement the `From` trait to allow converting your target struct, identified by
+            // `struct_identifier` to a HashMap with both the key and the value as `String`.
+            // Just like previously, #struct_identifier is replaced with the actual name of the
+            // target struct in output code.
             quote! {
                 #[automatically_derived]
                 impl From<#struct_identifier> for std::collections::HashMap<String, String> {
@@ -51,4 +57,39 @@ pub fn derive_into_hash_map(item: TokenStream) -> TokenStream {
         _ => unimplemented!(),
     }
     .into()
+}
+
+// function name does not matter
+// ... existing code ...
+
+#[proc_macro_derive(IntoHashMap)]
+pub fn derive_into_hash_map_v2(item: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(item as syn::DeriveInput);
+    let struct_identifier = &input.ident;
+
+    match &input.data {
+        Data::Struct(syn::DataStruct { fields, .. }) => {
+            let field_identifiers = fields.iter().map(|item| item.ident.as_ref().unwrap()).collect::<Vec<_>>();
+
+            quote! {
+                impl From<#struct_identifier> for std::collections::HashMap<String, String> {
+                    fn from(value: #struct_identifier) -> Self {
+                        let mut hash_map = std::collections::HashMap::<String, String>::new();
+
+                        #(
+                            hash_map.insert(stringify!(#field_identifiers).to_string(), String::from(value.#field_identifiers));
+                        )*
+
+                        hash_map
+                    }
+                }
+            }
+        }
+        _ => unimplemented!()
+    }.into()
+}
+
+#[proc_macro_derive(DeriveCustomModel, attributes(custom_model))]
+pub fn derive_custom_model(item: TokenStream) -> TokenStream {
+    custom_model::derive_custom_model_impl(item)
 }
--- my-app/src/main.rs
+++ my-app/src/main.rs
@@ -1,13 +1,27 @@
 // my-app/src/main.rs
 
-use my_app_macros::IntoStringHashMap;
+use my_app_macros::{DeriveCustomModel, IntoHashMap};
 use std::collections::HashMap;
 
-#[derive(IntoStringHashMap)]
+#[derive(IntoHashMap)]
 pub struct User {
     username: String,
     first_name: String,
     last_name: String,
+}
+
+#[derive(DeriveCustomModel)]
+#[custom_model(model(
+    name = "UserName",
+    fields(first_name, last_name),
+    extra_derives(IntoHashMap)
+))]
+#[custom_model(model(name = "UserInfo", fields(username, age), extra_derives(Debug)))]
+pub struct User2 {
+    username: String,
+    first_name: String,
+    last_name: String,
+    age: u32,
 }
 
 fn main() {
@@ -20,4 +34,12 @@ fn main() {
     let hash_map = HashMap::<String, String>::from(user);
 
     dbg!(hash_map);
+
+    // This user info field is derived from the DeriveCustomModel macros
+    let user_info = UserInfo {
+        username: "username".to_string(),
+        age: 27,
+    };
+
+    dbg!(user_info);
 }
